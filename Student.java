4eba3cb26b5c38646d1f6009b361b79106eb52fe
Madhu99599 <madhuprasadrongali@gmail.com>
2025-12-16 09:23:47 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -3,7 +3,7 @@ public class Student {
         int rollno = 1596;
         String sname = "KRISHNA";
         String course = "PYTHON"; 
-        System.out.println("LEARN HERE AND LEAD ANYWHERE!!!");
+        System.out.println("THIS IS BEST PLATFORM TO LEARN!!!");
         System.out.println("My Name is: " + sname);
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
